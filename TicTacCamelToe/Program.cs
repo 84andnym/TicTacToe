be33e3e2088a7c426379b76e8266f2f066872e8b
Andreas Nyman <70688270+84andnym@users.noreply.github.com>
2021-10-13 14:28:17 +0200

    new git ignore...

---
+++ TicTacCamelToe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicTacCamelToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
