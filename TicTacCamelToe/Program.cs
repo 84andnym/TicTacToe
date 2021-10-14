51e065959e99daa186bde747954317435a164b06
Tehpson <jesper@familjen-pettersson.se>
2021-10-14 10:07:19 +0200

    Added raylib

--- TicTacCamelToe/Program.cs
+++ TicTacCamelToe/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Raylib_cs;
+using System;
 
 namespace TicTacCamelToe
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Raylib.InitWindow(800, 480, "Hello World");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
         }
     }
 }
