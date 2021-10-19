23fb49796b3aed293152ba3af88052ad26e04d1a
Andreas Nyman <70688270+84andnym@users.noreply.github.com>
2021-10-19 10:51:14 +0200

    Added Win conditions

--- TicTacCamelToe/Grafic.cs
+++ TicTacCamelToe/Grafic.cs
@@ -9,19 +9,12 @@ namespace TicTacCamelToe
     {
         internal static void DrawGameBoard()
         {
-            var width = 1920;
-            var height = 1080;
-            int x = 0;
-            for (int i = 1; i < 4; i++)
+
+            for (int i = 0; i < Program.GameLogic.Rectangles.Count; i++)
             {
-                for (int y = 1; y < 4; y++)
-                {
-                    var rec = new Rectangle((width / 4 * i - (width / 5 / 2)), (height / 4 * y - (height / 5 / 2)), width / 5, height / 5);
-                    Raylib.DrawRectangleRec(rec, Program.GameLogic.GameBorad[x] == "O" ? Color.GREEN : Program.GameLogic.GameBorad[x] == "X" ? Color.RED : Color.GRAY);
-                    Program.GameLogic.Rectangles.Add(rec);
-                    x++;
-                }
+               Raylib.DrawRectangleRec(Program.GameLogic.Rectangles[i], Program.GameLogic.GameBorad[i] == "O" ? Color.GREEN : Program.GameLogic.GameBorad[i] == "X" ? Color.RED : Color.GRAY);
             }
         }
     }
 }
+
--- TicTacCamelToe/Program.cs
+++ TicTacCamelToe/Program.cs
@@ -1,18 +1,32 @@
 ﻿using Raylib_cs;
-using System;
 
 namespace TicTacCamelToe
 {
-    class Program
+    internal class Program
     {
         public static GameLogic GameLogic { get; set; }
-        static void Main()
+
+        private static void Main()
         {
-            Raylib.InitWindow(1920, 1080, "Tic tac toe");
+            var width = 1920;
+            var height = 1080;
+
+            Raylib.InitWindow(width, height, "Tic tac toe");
             Raylib.SetTargetFPS(60);
             //Raylib.ToggleFullscreen();
 
             GameLogic = new GameLogic();
+
+            int x = 0;
+            for (int i = 1; i < 4; i++)
+            {
+                for (int y = 1; y < 4; y++)
+                {
+                    var rec = new Rectangle((width / 4 * i - (width / 5 / 2)), (height / 4 * y - (height / 5 / 2)), width / 5, height / 5);
+                    GameLogic.Rectangles.Add(rec);
+                    x++;
+                }
+            }
 
             while (!Raylib.WindowShouldClose())
             {
