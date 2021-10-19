55cf9e7f0a8518a90503a1586bd9fc0ec4323976
Andreas Nyman <70688270+84andnym@users.noreply.github.com>
2021-10-19 10:10:40 +0200

    Added raylib mouse logic

--- TicTacCamelToe/Program.cs
+++ TicTacCamelToe/Program.cs
@@ -8,7 +8,7 @@ namespace TicTacCamelToe
         public static GameLogic GameLogic { get; set; }
         static void Main()
         {
-            Raylib.InitWindow(1920, 1080, "Tic tac cameltoe");
+            Raylib.InitWindow(1920, 1080, "Tic tac toe");
             Raylib.SetTargetFPS(60);
             Raylib.ToggleFullscreen();
 
@@ -19,9 +19,13 @@ namespace TicTacCamelToe
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.WHITE);
                 Grafic.DrawGameBoard();
-
-
                 Raylib.EndDrawing();
+
+                if (Raylib.IsMouseButtonReleased(MouseButton.MOUSE_LEFT_BUTTON))
+                {
+                    var pos = Raylib.GetMousePosition();
+                    GameLogic.GetRectangle(pos);
+                }
             }
             Raylib.CloseWindow();
         }
